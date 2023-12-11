aedbf036caafcfe9d6bfd2395a69b2d3bdcfb3b4
uzbmb <githubsobir@gmail.com>
2023-12-11 06:36:07 +0500

    home address

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() => runApp(const MaterialApp(
-    home: MainPage()
-    // home: MyApp(),
+    // home: MainPage()
+
+    home: MyApp(),
 ));
 
 class MyApp extends StatefulWidget {
